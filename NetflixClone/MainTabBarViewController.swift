74b4ff39b5004673b39fae1b8bbd1a623f8c7ed2
aldairmg <amartinezg7455@gmail.com>
2022-09-07 23:10:05 -0500

    Removed main.storyboard and added windowScene in SceneDelegate

---
+++ NetflixClone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarViewController.swift
+//  NetflixClone
+//
+//  Created by Aldair Martínez on 07/09/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
