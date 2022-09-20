ba1d79332185d861ff039d012261cf1b7108f0f4
Aldair Martinez <aldair.garcia@kavak.com>
2022-09-20 13:33:30 -0500

    :recycle: (navigationBar): Customizing the navigation bar

--- NetflixClone/Controller/HomeViewController.swift
+++ NetflixClone/Controller/HomeViewController.swift
@@ -75,6 +75,14 @@ extension HomeViewController: UITableViewDelegate {
         return 40
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+        
+    }
+    
 }
 
 extension HomeViewController: UITableViewDataSource {
